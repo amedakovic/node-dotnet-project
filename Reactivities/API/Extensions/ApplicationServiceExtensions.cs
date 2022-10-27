fd449654ebbf578cd72cf84230d8a972964da01c
amedakovic <medakovic.aleks1@gmail.com>
2022-10-27 18:22:15 +0200

    Added mediator and crud to api

---
+++ Reactivities/API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration _config)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+            });
+            services.AddDbContext<Persistance.DataContext>(opt =>
+            {
+                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            }
+            );
+
+            services.AddCors(opt =>{
+                opt.AddPolicy("CorsPolicy",policy =>{
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3001");
+                });
+            });
+
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
