03fd1b240142460750977ad7b8323b77e084b886
amedakovic <medakovic.aleks1@gmail.com>
2022-10-28 18:13:13 +0200

    Added nav bar and some extras for future

---
+++ Reactivities/client-app/src/app/layout/NavBar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button, Container, Menu } from "semantic-ui-react";
+
+export default function NavBar(){
+    return(
+        <Menu inverted fixed="top">
+            <Container>
+                <Menu.Item header>
+                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
+                     Reactivites
+                </Menu.Item>
+                <Menu.Item name='activties'/>
+                <Menu.Item>
+                    <Button positive content='Create activity'/>
+                </Menu.Item>
+            </Container>
+
+        </Menu>
+    )
+}
---
+++ Reactivities/client-app/src/app/models/activity.ts
@@ -0,0 +1,9 @@
+export interface Activity{
+    id: string;
+    title: string;
+    data: string;
+    desciption: string;
+    category: string;
+    city: string;
+    venue: string;
+}
